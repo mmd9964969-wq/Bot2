d454d35a6afc929a33fd743ea28f76912b2e013d
mmd9964969-wq <mmd9964969@gmail.com>
2026-09-18 07:30:21 +0330

    Fix Telegram API import path

--- src/routes/api/bot/action.ts
+++ src/routes/api/bot/action.ts
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { telegramApi } from "../../../../lib/telegram/api.ts";
+import { telegramApi } from "../../../lib/telegram/api.ts";
 
 export const Route = createFileRoute("/api/bot/action")({
   server: {
